3be55ad340c5aae00b5d4fc5a09dc881730a92eb
shre-555 <shreyakundu6782@gmail.com>
2023-11-25 11:48:05 +0530

    artistic.py added

--- ui.py
+++ ui.py
@@ -4,13 +4,16 @@ from tkinter import filedialog
 from tkinter.filedialog import askopenfile
 from PIL import Image, ImageTk
 import customtkinter as ctk
+import numpy as np
+import artistic as art
+import cv2
 
 root=ctk.CTk()
 ctk.set_appearance_mode("Dark")
 ctk.set_default_color_theme("dark-blue")
 root.geometry("800x800")
 root.title("Image to Sketch")
-my_font1=('Constantia', 18, 'bold')
+my_font1=('Constantia', 18)
 
 
 def upload_file():
@@ -19,24 +22,45 @@ def upload_file():
     filename = tk.filedialog.askopenfilename(filetypes=f_types)
     img=Image.open(filename) # read the image file
     img=img.resize((100,100)) # new width & height
-    img=ImageTk.PhotoImage(img) #to convert to format tkinter can use
+    img1=ImageTk.PhotoImage(img) #to convert to format tkinter can use
     e1 =tk.Label(mainbar)
     e1.grid(row=3,column=2)
-    e1.image = img # keep a reference! by attaching it to a widget attribute
-    e1['image']=img # Show Image
+    e1.image = img1 # keep a reference! by attaching it to a widget attribute
+    e1['image']=img1 # Show Image
+
+#org_img= cv2.imdecode(np.fromstring(filename.read(), np.uint8), 1)
+
+'''
+def pencil1():
+    global org_img
+    res=art.sketch(org_img)
+    res_img=Image.fromarray(org_img)
+    res_img=ImageTk.PhotoImage(res_img)
+    e2 =tk.Label(mainbar)
+    e2.grid(row=3,column=3)
+    e2.image = res_img # keep a reference! by attaching it to a widget attribute
+    e2['image']=res_img # Show Image
+'''
+
+sidebar=ctk.CTkFrame(master=root, width=100)
+
+def side():
+    sidebar.grid_columnconfigure(0, weight=1)
+    intro=ctk.CTkLabel(sidebar, text="Image to Sketch", font=('Constantia', 18, 'bold'))
+    intro.grid(row=0,column=0, padx=10, pady=5, sticky="nsew")
+    pencil_button=ctk.CTkButton(sidebar, text='Pencil Sketch',command = lambda:pencil1())
+    pencil_button.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
+
+side()
+sidebar.pack(side=tk.LEFT,fill='both',expand=True, padx=5, pady=5)
+mainbar=ctk.CTkScrollableFrame(master=root, width=700, orientation="horizontal")
+mainbar.pack(side=tk.LEFT,fill='both',expand=True, padx=10,pady=5)
 
 
-sidebar=ctk.CTkFrame(master=root, width=100, fg_color='gray')
-sidebar.pack(side=tk.LEFT,fill='both',expand=True)
-mainbar=ctk.CTkFrame(master=root, width=500, fg_color='black')
-mainbar.pack(side=tk.LEFT,fill='both',expand=True)
-
-#mainbar.grid_rowconfigure(1, weight=1)
-#mainbar.grid_columnconfigure(1, weight=1)
 l1 = ctk.CTkLabel(mainbar,text='Upload Files & display',width=30,font=my_font1)  
 l1.grid(row=1,column=2,columnspan=4,sticky="nsew",padx=10, pady=5)
-b1 = ctk.CTkButton(mainbar, text='Upload Files', 
-   width=20,command = lambda:upload_file())
+#icon = PhotoImage(file=r"C:\Users\ROSHNI\OneDrive\Pictures\upload_icon.png")
+b1 = ctk.CTkButton(mainbar, text='Upload Files', width=20,command = lambda:upload_file())
 b1.grid(row=2,column=1,columnspan=4,sticky="nsew",padx=10, pady=5)
 
 
