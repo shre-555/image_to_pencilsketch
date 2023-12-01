7ce1449cbb270541e783c686195c30e1153554a1
shre-555 <shreyakundu6782@gmail.com>
2023-12-01 21:16:07 +0530

    Disable and enable button feature

--- ui.py
+++ ui.py
@@ -14,11 +14,17 @@ ctk.set_default_color_theme("dark-blue")
 root.geometry("800x800")
 root.title("Image to Sketch")
 my_font1=('Constantia', 18)
+is_enabled=False
+image = None
+colour_button = None
 
 def upload():
     global panelA, panelB, image
     f_types = [('Jpg Files', '*.jpg'),('PNG Files','*.png')] 
     path = filedialog.askopenfilename(filetypes=f_types)
+
+    is_enabled = True
+    
     image = cv2.imread(path) 
     image = cv2.resize(image, (500,500))
 
@@ -32,11 +38,17 @@ def upload():
     panelA.grid(row= 3, column=2 , padx=20, pady=20)
     text= ctk.CTkLabel(mainbar,text='Original Image', font=my_font1)
     text.grid(row= 4, column=2, padx=20, pady=20)
+
+    colour_button.configure(state="normal")
+
     return image
 
 
 
 def colourpencil():
+    if image is None:
+        print("Upload an image")
+        return
     cps= cpsk.colour_sketch(image)
     pencil= Image.fromarray(cps)
     pencil= ImageTk.PhotoImage(pencil)
@@ -59,12 +71,18 @@ b1.grid(row=2,column=0,sticky="nsew",padx=10, pady=5)
 sidebar=ctk.CTkFrame(master=root, width=100)
 
 def side():
+    global colour_button
     sidebar.grid_columnconfigure(0, weight=1)
     intro=ctk.CTkLabel(sidebar, text="Image to Sketch", font=('Constantia', 18, 'bold'))
     intro.grid(row=0,column=0, padx=10, pady=5, sticky="nsew")
     #pencil_button=ctk.CTkButton(sidebar, text='Pencil Sketch',command = pencil)
     #pencil_button.grid(row=1, column=0, padx=20, pady=20, sticky="nsew")
-    colour_button=ctk.CTkButton(sidebar, text='Colour Pencil Sketch',command = colourpencil)
+    colour_button=ctk.CTkButton(sidebar, text='Colour Pencil Sketch', command = colourpencil)
+    print(is_enabled)
+    if is_enabled:
+        colour_button.configure(state="normal")
+    else:
+        colour_button.configure(state="disabled")
     colour_button.grid(row=2, column=0, padx=20, pady=20, sticky="nsew")
 
 side()
