8ab289526a3261c8133d084463309593aff7ce30
Siri64 <151047839+Siri64@users.noreply.github.com>
2023-12-07 17:15:32 +0530

    Create pencil_sketch.py

---
+++ pencil_sketch.py
@@ -0,0 +1,21 @@
+import numpy as np
+import imageio
+import scripy.ndimage
+import cv2
+import matplotlib.pyplot as plt
+
+def rgb2gray(rgb):
+	return np.dot(rgb[..., :3], [0.2989, 0.5870, .1140])
+
+def dodge(front, back):
+	final_sketch = front*255/(255-back)
+	final_sketch[final_sketch > 255] = 255    
+	final_sketch[back == 255] = 255	
+	return final_sketch.astype('uint8')
+
+def pencil(img):
+    gray = rgb2gray(img)
+    i = 255-gray
+    blur = scipy.ndimage.filters.gaussian_filter(i, sigma=13)
+    r = dodge(blur, gray)
+    return r
