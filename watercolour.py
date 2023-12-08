d90f747e4b8485a285188e5b161310c56f78a7be
ShrutiPathak111 <152715108+ShrutiPathak111@users.noreply.github.com>
2023-12-09 02:12:12 +0530

    Update watercolour.py

--- watercolour.py
+++ watercolour.py
@@ -1,11 +1,7 @@
 import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread('img.jpg')
-original_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-plt.axis("off")
-plt.imshow(original_img)
-plt.axis("off")
-watercolour_image = cv2.stylization(original_img, sigma_s=100, sigma_r=0.45)
-plt.imshow(watercolour_image)
-cv2.imwrite('sketch1.png',watercolour_image)
-True
+import numpy as np
+
+def oil_sketch(img, d=9, sigmaColor=75, sigmaSpace=75, brightness_factor=0.7, contrast_factor=1.3):
+    watercolor_image = cv2.bilateralFilter(img, d=d, sigmaColor=sigmaColor, sigmaSpace=sigmaSpace)
+    watercolor_image = cv2.convertScaleAbs(watercolor_image, alpha=contrast_factor, beta=brightness_factor)
+    return watercolor_image
